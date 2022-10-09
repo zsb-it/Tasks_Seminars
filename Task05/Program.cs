ade67264209ee35d0ba002531715f18b320e8452
Dmitry Kireev <zzsb@ya.ru>
2022-10-09 14:50:00 +0300

    Solution Task05

---
+++ Task05/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая
+// 1. на вход принимает число (N), а
+// 2. на выходе показывает все целые числа в промежутке от -N до N.
+
+// 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+// 2 -> "-2, -1, 0, 1, 2"
+
+Console.WriteLine("Введите целое число:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int counter = -number;
+while (counter <= number)
+{
+    Console.Write($"{counter} ");
+    counter++;
+}
+
