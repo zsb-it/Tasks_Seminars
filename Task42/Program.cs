e4d2c301d22d9dbae64165e7c3230fc0db7855b8
Dmitry Kireev <zzsb@ya.ru>
2022-10-18 21:38:44 +0300

    Task42change

--- Task42/Program.cs
+++ Task42/Program.cs
@@ -8,6 +8,8 @@
 Console.WriteLine("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
+// первый вариант
+
 int Binary(int num)
 {
     int temp = 0;
@@ -23,3 +25,19 @@ int Binary(int num)
 
 int binary = Binary(number);
 Console.WriteLine($"{number} -> {binary}");
+
+// второй вариант
+
+// string DecToBin(int number)
+// {
+//     string result = String.Empty;
+//     while (number > 0)
+//     {
+//         result = Convert.ToString(number % 2) + result;
+//         number = number / 2;
+//     }
+//     return result;
+// }
+
+// string decToBin = DecToBin(num);
+// Console.WriteLine(decToBin);
