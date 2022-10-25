5945436052e083c64a743c2c3242c269a0ef74b5
Dmitry Kireev <zzsb@ya.ru>
2022-10-25 09:15:05 +0300

    Task08_HW

---
+++ Task08_HW/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Введите положительное число больше 1: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Чётные числа от 1 до {number}: ");
+
+int count = 2;
+while (count <= number)
+{
+    Console.Write($"{count} ");
+    count = count + 2;
+}
