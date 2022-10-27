883153093515f3715867827b530722fb01bfc323
Dmitry Kireev <zzsb@ya.ru>
2022-10-27 20:23:17 +0300

    Task41 & Task43

---
+++ Task41_HW/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+// Примечание: Попытался решить задачу, используя методы .Split и .Parse
+
+Console.Write("Введите несколько чисел через запятую:");
+
+string[] numbersMassive;                                // Создаем строковый массив
+string? number = Console.ReadLine();                    // Получаем от пользователя набор цифр
+
+// Заполняем массив полученными цифрами, используя в качестве разделителя ","
+numbersMassive = number.Split(',');                     // Что значит "разыменование пустой ссылки"?                  
+
+int[] countNumbers = new int[numbersMassive.Length];    // Создаем числовой массив размером со строковый
+
+void NumMassive(int[] stringArray)                      // Метод преобразования строкового массива в числовой
+{
+    for (int i = 0; i < numbersMassive.Length; i++)
+        countNumbers[i] = int.Parse(numbersMassive[i]);
+}
+
+NumMassive(countNumbers);                               // Преобразуем строковый массив в числовой
+
+int CountPositive(int[] numArray)                       // Метод подсчета количества положительных чисел в массиве
+{
+    int countPositive = 0;
+    for (int i = 0; i < countNumbers.Length; i++)
+        if (countNumbers[i] > 0) countPositive++;
+    return countPositive;
+}
+
+int count = CountPositive(countNumbers);                // Подсчитываем количество положительных чисел в массиве
+
+Console.Write(count);                                   // Выводим количество положительных чисел в консоль
---
+++ Task43_HW/Program.cs
@@ -0,0 +1,38 @@
+﻿// Напишите программу, которая
+// 1. найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.Write("Введите b1:");
+int numberB1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите k1:");
+int numberK1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите b2:");
+int numberB2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите k2:");
+int numberK2 = Convert.ToInt32(Console.ReadLine());
+
+double[] IntersectionPoint(double B1, double K1, double B2, double K2)
+{
+    int size = 2;
+    double[] point = new double[size];
+    int i = 0;
+
+    point[i] = (B2 - B1) / (K1 - K2);
+    i++;
+    point[i] = K1 * point[i - 1] + B1;
+
+    return point;
+}
+
+void PrintIntersectionPoint(double[] intersectionPoint)
+{
+    int i=0;
+    Console.Write($"b1 = {numberB1}, k1 = {numberK1}, b2 = {numberB2}, k2 = {numberK2}");
+    Console.Write($" - > ({intersectionPoint[i]} ; {intersectionPoint[i++]})");
+}
+
+IntersectionPoint(numberB1, numberK1, numberB2, numberK2);
+PrintIntersectionPoint(IntersectionPoint(numberB1, numberK1, numberB2, numberK2));
