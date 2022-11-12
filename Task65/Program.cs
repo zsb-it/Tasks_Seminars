34c538ede6908d25e2496d5a57a4e53a7d62121d
Dmitry Kireev <zzsb@ya.ru>
2022-11-12 20:37:47 +0300

    Task65, 67, 69

---
+++ Task65/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 65: Задайте значения M и N.
+// Напишите программу, которая
+// выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 6, 7, 8"
+
+Console.WriteLine("Введите целое число M:");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите целое число N:");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+void NaturalNumbers(int numM, int numN)
+{
+
+    if (numM == numN) Console.Write($"{numM} ");
+    
+    if (numM < numN)
+    {
+        Console.Write($"{numM} ");
+        NaturalNumbers(numM + 1, numN);
+    }
+    if (numM > numN)
+    {
+        Console.Write($"{numM} ");
+        NaturalNumbers(numM - 1, numN);
+    }
+
+}
+NaturalNumbers(numberM, numberN);
---
+++ Task69/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 69: Напишите программу, которая на вход
+// принимает два числа A и B, и
+// возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+Console.WriteLine("Введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int Power (int a, int b)
+{
+    if (b == 0) return 1;
+    else return a * Power (a, b-1);
+}
+
+int res = Power(numberA, numberB);
+Console.WriteLine(res);
